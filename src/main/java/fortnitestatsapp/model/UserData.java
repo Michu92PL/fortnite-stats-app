70cac28f30a9d0d6805276263b09b2f4fbe4ccc9
Michu <mich1992@wp.pl>
2019-01-23 16:47:17 +0100

    Added UserData model class

---
+++ src/main/java/fortnitestatsapp/model/UserData.java
@@ -0,0 +1,398 @@
+package fortnitestatsapp.model;
+
+public class UserData {
+
+        private String accountID;
+        private String epicName;
+
+        private String totalWins;
+        private String totalScore;
+        private String totalMatchesPlayed;
+        private String totalWinPercentage;
+        private String totalKills;
+        private String totalKD;
+
+        private String soloWins;
+        private String soloKD;
+        private String soloWinPercentage;
+        private String soloGamesPlayed;
+        private String soloKills;
+
+        private String duoWins;
+        private String duoKD;
+        private String duoWinPercentage;
+        private String duoGamesPlayed;
+        private String duoKills;
+
+        private String teamWins;
+        private String teamKD;
+        private String teamWinPercentage;
+        private String teamGamesPlayed;
+        private String teamKills;
+
+        private String currentSoloWins;
+        private String currentSoloKD;
+        private String currentSoloWinPercentage;
+        private String currentSoloGamesPlayed;
+        private String currentSoloKills;
+
+        private String currentDuoWins;
+        private String currentDuoKD;
+        private String currentDuoWinPercentage;
+        private String currentDuoGamesPlayed;
+        private String currentDuoKills;
+
+        private String currentTeamWins;
+        private String currentTeamKD;
+        private String currentTeamWinPercentage;
+        private String currentTeamGamesPlayed;
+        private String currentTeamKills;
+
+    public String getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(String accountID) {
+        this.accountID = accountID;
+    }
+
+    public String getEpicName() {
+        return epicName;
+    }
+
+    public void setEpicName(String epicName) {
+        this.epicName = epicName;
+    }
+
+    public String getTotalWins() {
+        return totalWins;
+    }
+
+    public void setTotalWins(String totalWins) {
+        this.totalWins = totalWins;
+    }
+
+    public String getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(String totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public String getTotalMatchesPlayed() {
+        return totalMatchesPlayed;
+    }
+
+    public void setTotalMatchesPlayed(String totalMatchesPlayed) {
+        this.totalMatchesPlayed = totalMatchesPlayed;
+    }
+
+    public String getTotalWinPercentage() {
+        return totalWinPercentage;
+    }
+
+    public void setTotalWinPercentage(String totalWinPercentage) {
+        this.totalWinPercentage = totalWinPercentage;
+    }
+
+    public String getTotalKills() {
+        return totalKills;
+    }
+
+    public void setTotalKills(String totalKills) {
+        this.totalKills = totalKills;
+    }
+
+    public String getTotalKD() {
+        return totalKD;
+    }
+
+    public void setTotalKD(String totalKD) {
+        this.totalKD = totalKD;
+    }
+
+    public String getSoloWins() {
+        return soloWins;
+    }
+
+    public void setSoloWins(String soloWins) {
+        this.soloWins = soloWins;
+    }
+
+    public String getSoloKD() {
+        return soloKD;
+    }
+
+    public void setSoloKD(String soloKD) {
+        this.soloKD = soloKD;
+    }
+
+    public String getSoloWinPercentage() {
+        return soloWinPercentage;
+    }
+
+    public void setSoloWinPercentage(String soloWinPercentage) {
+        this.soloWinPercentage = soloWinPercentage;
+    }
+
+    public String getSoloGamesPlayed() {
+        return soloGamesPlayed;
+    }
+
+    public void setSoloGamesPlayed(String soloGamesPlayed) {
+        this.soloGamesPlayed = soloGamesPlayed;
+    }
+
+    public String getSoloKills() {
+        return soloKills;
+    }
+
+    public void setSoloKills(String soloKills) {
+        this.soloKills = soloKills;
+    }
+
+    public String getDuoWins() {
+        return duoWins;
+    }
+
+    public void setDuoWins(String duoWins) {
+        this.duoWins = duoWins;
+    }
+
+    public String getDuoKD() {
+        return duoKD;
+    }
+
+    public void setDuoKD(String duoKD) {
+        this.duoKD = duoKD;
+    }
+
+    public String getDuoWinPercentage() {
+        return duoWinPercentage;
+    }
+
+    public void setDuoWinPercentage(String duoWinPercentage) {
+        this.duoWinPercentage = duoWinPercentage;
+    }
+
+    public String getDuoGamesPlayed() {
+        return duoGamesPlayed;
+    }
+
+    public void setDuoGamesPlayed(String duoGamesPlayed) {
+        this.duoGamesPlayed = duoGamesPlayed;
+    }
+
+    public String getDuoKills() {
+        return duoKills;
+    }
+
+    public void setDuoKills(String duoKills) {
+        this.duoKills = duoKills;
+    }
+
+    public String getTeamWins() {
+        return teamWins;
+    }
+
+    public void setTeamWins(String teamWins) {
+        this.teamWins = teamWins;
+    }
+
+    public String getTeamKD() {
+        return teamKD;
+    }
+
+    public void setTeamKD(String teamKD) {
+        this.teamKD = teamKD;
+    }
+
+    public String getTeamWinPercentage() {
+        return teamWinPercentage;
+    }
+
+    public void setTeamWinPercentage(String teamWinPercentage) {
+        this.teamWinPercentage = teamWinPercentage;
+    }
+
+    public String getTeamGamesPlayed() {
+        return teamGamesPlayed;
+    }
+
+    public void setTeamGamesPlayed(String teamGamesPlayed) {
+        this.teamGamesPlayed = teamGamesPlayed;
+    }
+
+    public String getTeamKills() {
+        return teamKills;
+    }
+
+    public void setTeamKills(String teamKills) {
+        this.teamKills = teamKills;
+    }
+
+    public String getCurrentSoloWins() {
+        return currentSoloWins;
+    }
+
+    public void setCurrentSoloWins(String currentSoloWins) {
+        this.currentSoloWins = currentSoloWins;
+    }
+
+    public String getCurrentSoloKD() {
+        return currentSoloKD;
+    }
+
+    public void setCurrentSoloKD(String currentSoloKD) {
+        this.currentSoloKD = currentSoloKD;
+    }
+
+    public String getCurrentSoloWinPercentage() {
+        return currentSoloWinPercentage;
+    }
+
+    public void setCurrentSoloWinPercentage(String currentSoloWinPercentage) {
+        this.currentSoloWinPercentage = currentSoloWinPercentage;
+    }
+
+    public String getCurrentSoloGamesPlayed() {
+        return currentSoloGamesPlayed;
+    }
+
+    public void setCurrentSoloGamesPlayed(String currentSoloGamesPlayed) {
+        this.currentSoloGamesPlayed = currentSoloGamesPlayed;
+    }
+
+    public String getCurrentSoloKills() {
+        return currentSoloKills;
+    }
+
+    public void setCurrentSoloKills(String currentSoloKills) {
+        this.currentSoloKills = currentSoloKills;
+    }
+
+    public String getCurrentDuoWins() {
+        return currentDuoWins;
+    }
+
+    public void setCurrentDuoWins(String currentDuoWins) {
+        this.currentDuoWins = currentDuoWins;
+    }
+
+    public String getCurrentDuoKD() {
+        return currentDuoKD;
+    }
+
+    public void setCurrentDuoKD(String currentDuoKD) {
+        this.currentDuoKD = currentDuoKD;
+    }
+
+    public String getCurrentDuoWinPercentage() {
+        return currentDuoWinPercentage;
+    }
+
+    public void setCurrentDuoWinPercentage(String currentDuoWinPercentage) {
+        this.currentDuoWinPercentage = currentDuoWinPercentage;
+    }
+
+    public String getCurrentDuoGamesPlayed() {
+        return currentDuoGamesPlayed;
+    }
+
+    public void setCurrentDuoGamesPlayed(String currentDuoGamesPlayed) {
+        this.currentDuoGamesPlayed = currentDuoGamesPlayed;
+    }
+
+    public String getCurrentDuoKills() {
+        return currentDuoKills;
+    }
+
+    public void setCurrentDuoKills(String currentDuoKills) {
+        this.currentDuoKills = currentDuoKills;
+    }
+
+    public String getCurrentTeamWins() {
+        return currentTeamWins;
+    }
+
+    public void setCurrentTeamWins(String currentTeamWins) {
+        this.currentTeamWins = currentTeamWins;
+    }
+
+    public String getCurrentTeamKD() {
+        return currentTeamKD;
+    }
+
+    public void setCurrentTeamKD(String currentTeamKD) {
+        this.currentTeamKD = currentTeamKD;
+    }
+
+    public String getCurrentTeamWinPercentage() {
+        return currentTeamWinPercentage;
+    }
+
+    public void setCurrentTeamWinPercentage(String currentTeamWinPercentage) {
+        this.currentTeamWinPercentage = currentTeamWinPercentage;
+    }
+
+    public String getCurrentTeamGamesPlayed() {
+        return currentTeamGamesPlayed;
+    }
+
+    public void setCurrentTeamGamesPlayed(String currentTeamGamesPlayed) {
+        this.currentTeamGamesPlayed = currentTeamGamesPlayed;
+    }
+
+    public String getCurrentTeamKills() {
+        return currentTeamKills;
+    }
+
+    public void setCurrentTeamKills(String currentTeamKills) {
+        this.currentTeamKills = currentTeamKills;
+    }
+
+    @Override
+    public String toString() {
+        return "UserData{" +
+                "accountID='" + accountID + '\'' +
+                ", epicName='" + epicName + '\'' +
+                ", totalWins='" + totalWins + '\'' +
+                ", totalScore='" + totalScore + '\'' +
+                ", totalMatchesPlayed='" + totalMatchesPlayed + '\'' +
+                ", totalWinPercentage='" + totalWinPercentage + '\'' +
+                ", totalKills='" + totalKills + '\'' +
+                ", totalKD='" + totalKD + '\'' +
+                ", soloWins='" + soloWins + '\'' +
+                ", soloKD='" + soloKD + '\'' +
+                ", soloWinPercentage='" + soloWinPercentage + '\'' +
+                ", soloGamesPlayed='" + soloGamesPlayed + '\'' +
+                ", soloKills='" + soloKills + '\'' +
+                ", duoWins='" + duoWins + '\'' +
+                ", duoKD='" + duoKD + '\'' +
+                ", duoWinPercentage='" + duoWinPercentage + '\'' +
+                ", duoGamesPlayed='" + duoGamesPlayed + '\'' +
+                ", duoKills='" + duoKills + '\'' +
+                ", teamWins='" + teamWins + '\'' +
+                ", teamKD='" + teamKD + '\'' +
+                ", teamWinPercentage='" + teamWinPercentage + '\'' +
+                ", teamGamesPlayed='" + teamGamesPlayed + '\'' +
+                ", teamKills='" + teamKills + '\'' +
+                ", currentSoloWins='" + currentSoloWins + '\'' +
+                ", currentSoloKD='" + currentSoloKD + '\'' +
+                ", currentSoloWinPercentage='" + currentSoloWinPercentage + '\'' +
+                ", currentSoloGamesPlayed='" + currentSoloGamesPlayed + '\'' +
+                ", currentSoloKills='" + currentSoloKills + '\'' +
+                ", currentDuoWins='" + currentDuoWins + '\'' +
+                ", currentDuoKD='" + currentDuoKD + '\'' +
+                ", currentDuoWinPercentage='" + currentDuoWinPercentage + '\'' +
+                ", currentDuoGamesPlayed='" + currentDuoGamesPlayed + '\'' +
+                ", currentDuoKills='" + currentDuoKills + '\'' +
+                ", currentTeamWins='" + currentTeamWins + '\'' +
+                ", currentTeamKD='" + currentTeamKD + '\'' +
+                ", currentTeamWinPercentage='" + currentTeamWinPercentage + '\'' +
+                ", currentTeamGamesPlayed='" + currentTeamGamesPlayed + '\'' +
+                ", currentTeamKills='" + currentTeamKills + '\'' +
+                '}';
+    }
+}
