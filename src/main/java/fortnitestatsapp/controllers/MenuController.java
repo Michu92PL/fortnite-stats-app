6a48cd0eae56ccc9b6f765b6e9a4bde0e464fc45
Michu <mich1992@wp.pl>
2019-01-29 16:16:28 +0100

    Added MenuController class, and implemented action methods for all buttons

---
+++ src/main/java/fortnitestatsapp/controllers/MenuController.java
@@ -0,0 +1,50 @@
+package fortnitestatsapp.controllers;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+public class MenuController {
+
+    private BootController bootController;
+
+
+    public void openStats() {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/StatsView.fxml"));
+        Pane pane = null;
+        try{
+            pane = loader.load();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        bootController.setScreen(pane);
+
+    }
+
+    public void openComparison() {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/ComparisonView.fxml"));
+        Pane pane = null;
+        try{
+            pane = loader.load();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        bootController.setScreen(pane);
+
+    }
+
+    public void exit() {
+        Platform.exit();
+        System.exit(0);
+
+    }
+
+    public void setBootController(BootController bootController) {
+        this.bootController = bootController;
+    }
+}
