51523e67676c8c86543fa7e4a09032e4e8c3e851
Michu <mich1992@wp.pl>
2019-02-05 21:20:29 +0100

    Added ComparisonController class

---
+++ src/main/java/fortnitestatsapp/controllers/ComparisonController.java
@@ -0,0 +1,13 @@
+package fortnitestatsapp.controllers;
+
+import fortnitestatsapp.model.UserData;
+
+public class ComparisonController {
+
+    private UserData player1;
+    private UserData player2;
+
+    public void setPlayer1(UserData player1) {
+        this.player1 = player1;
+    }
+}
