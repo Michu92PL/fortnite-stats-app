5ec478f801febcb43bafc85080727494004a9d0a
Michu <mich1992@wp.pl>
2019-02-19 05:33:45 +0100

    Changed how program behave when more stages are opened and main stage is closed

--- src/main/java/fortnitestatsapp/main/Main.java
+++ src/main/java/fortnitestatsapp/main/Main.java
@@ -1,8 +1,7 @@
 package fortnitestatsapp.main;
 
-import fortnitestatsapp.model.UserData;
-import fortnitestatsapp.service.Service;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -23,6 +22,7 @@ public class Main extends Application {
         primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("/img/icon.png")));
 
         //primaryStage.resizableProperty().setValue(Boolean.FALSE);
+        primaryStage.setOnCloseRequest(event -> Platform.exit());
         primaryStage.setScene(scene);
         primaryStage.setMinWidth(800);
         primaryStage.setMaxWidth(800);
