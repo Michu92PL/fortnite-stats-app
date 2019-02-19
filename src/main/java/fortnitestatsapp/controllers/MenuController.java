4094178746ad9ca425f5cb08835b30968866efe6
Michu <mich1992@wp.pl>
2019-02-19 05:29:24 +0100

    Added openAbout method, minor changes

--- src/main/java/fortnitestatsapp/controllers/MenuController.java
+++ src/main/java/fortnitestatsapp/controllers/MenuController.java
@@ -1,7 +1,8 @@
 package fortnitestatsapp.controllers;
 
+import fortnitestatsapp.dialogs.DialogUtils;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
 
@@ -9,7 +10,7 @@ public class MenuController {
 
     private BootController bootController;
 
-
+    @FXML
     public void openStats() {
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/StatsView.fxml"));
         Pane pane = null;
@@ -17,14 +18,31 @@ public class MenuController {
             pane = loader.load();
         }
         catch (Exception e){
-            e.printStackTrace();
+            //e.printStackTrace();
+            DialogUtils.errorDialog(e);
         }
         StatsController statsController = loader.getController();
         statsController.setBootController(bootController);
         bootController.setScreen(pane);
-
     }
 
+    @FXML
+    public void openAbout(){
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/AboutView.fxml"));
+        Pane pane = null;
+        try{
+            pane = loader.load();
+        }
+        catch (Exception e){
+            //e.printStackTrace();
+            DialogUtils.errorDialog(e);
+        }
+        AboutController aboutController = loader.getController();
+        aboutController.setBootController(bootController);
+        bootController.setScreen(pane);
+    }
+
+    @FXML
     public void openComparison() {
         bootController.openComparison();
     }
@@ -32,7 +50,6 @@ public class MenuController {
     public void exit() {
         Platform.exit();
         System.exit(0);
-
     }
 
     public void setBootController(BootController bootController) {
